6e96b41e3958329e4af9de140e2e9c27efde1a05
Rafael Moret <rafaelmoretgalan@gmail.com>
2022-07-08 15:10:15 +0200

    Fix typos and type errors. Add random_state param

--- transformer_pack/base.py
+++ transformer_pack/base.py
@@ -1,14 +1,20 @@
+import os
 import numpy as np
 import pandas as pd
+
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 import tensorflow as tf
 
 from sklearn.base import TransformerMixin, BaseEstimator
 
 
 class CategoricalEmbedding(BaseEstimator, TransformerMixin):
-    def __init__(self, *, embedding_dim="auto", objective="binary-class"):
+    def __init__(self, *, embedding_dim="auto", objective=None, random_state=None):
         self.embedding_dim = embedding_dim
         self.objective = objective
+        self.random_state = random_state
+
+        tf.random.set_seed(self.random_state)
 
     def fit(self, X, y=None):
         X = X.copy()
@@ -19,18 +25,19 @@ class CategoricalEmbedding(BaseEstimator, TransformerMixin):
 
         self.embeddings_ = []
         for i_col in range(n_features):
-            feature = X[:, i_col].copy()
+            feature = X[:, i_col].astype(str).copy()
 
             model = self._embedding_model(feature)
-            model.fit(feature, y)
+            model.fit(feature, y, verbose=0)
 
             feature_embeddings = {
-                key: embedding
+                key: list(embedding)
                 for key, embedding in zip(
                     ["[UNK]"] + list(np.unique(feature)),
                     model.layers[2].get_weights()[0],
                 )
             }
+
             self.embeddings_.append(feature_embeddings)
 
         return self
@@ -43,9 +50,9 @@ class CategoricalEmbedding(BaseEstimator, TransformerMixin):
         n_uniques = len(vocab)
 
         if self.embedding_dim == "auto":
-            feature_embedding_dim = np.sqrt(n_uniques)
+            feature_embedding_dim = int(np.sqrt(n_uniques))
         else:
-            feature_embedding_dim = self.embedding_dim
+            feature_embedding_dim = int(self.embedding_dim)
 
         if self.objective == "binary-class":
             activation_fun = "sigmoid"
@@ -61,11 +68,12 @@ class CategoricalEmbedding(BaseEstimator, TransformerMixin):
                 "'multi-class' or 'regression', depending on your problem."
             )
 
-        input_layer = tf.keras.layers.Input(shape=(1,))
+        input_layer = tf.keras.layers.Input(shape=(1,), dtype='string')
         embedding_nn = tf.keras.layers.StringLookup(vocabulary=vocab)(input_layer)
         embedding_nn = tf.keras.layers.Embedding(
             output_dim=feature_embedding_dim, input_dim=n_uniques + 1, input_length=1
         )(embedding_nn)
+        embedding_nn = tf.keras.layers.Flatten()(embedding_nn)
         embedding_nn = tf.keras.layers.Dense(units=128, activation="relu")(embedding_nn)
         embedding_nn = tf.keras.layers.Dense(units=1, activation=activation_fun)(
             embedding_nn
