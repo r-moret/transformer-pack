71b8afc0e74d791a112bc881e5f2d96eeacbd85d
Rafael Moret <rafaelmoretgalan@gmail.com>
2022-07-08 14:13:43 +0200

    Rename package. Substitute '-' by '_'

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="transformer-pack",
-    packages=find_packages(include=["transformer-pack"]),
+    packages=find_packages(include=["transformer_pack"]),
     version="0.1.0",
     description="Package of extra scikit-learn transformers",
     author="Rafael Moret",
